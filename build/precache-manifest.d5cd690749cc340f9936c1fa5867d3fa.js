daabe686a6b91bdc56a8e28a1596ff56fd5b82db
Samartha Mudigere”;git config --global user.name Samartha <samarthamudigere@hotmail.com>
2019-06-08 10:48:29 -0500

    Build 1

---
+++ build/precache-manifest.d5cd690749cc340f9936c1fa5867d3fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59790129f19166b6d7fc62d3c972d9fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb293ec752acb813d6e",
+    "url": "/static/css/main.d4969cb2.chunk.css"
+  },
+  {
+    "revision": "c439baa03007fa94c969",
+    "url": "/static/js/2.966dd194.chunk.js"
+  },
+  {
+    "revision": "acb293ec752acb813d6e",
+    "url": "/static/js/main.9dda0e97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
